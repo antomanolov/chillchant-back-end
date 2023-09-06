d5da3bce245d4246a2e834175426c3d716092ef4
Anton Manolov <mramanolov@gmail.com>
2023-09-06 14:01:16 +0300

    added custom auth form, login is functional

--- app_profiles/forms.py
+++ app_profiles/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 
 from app_profiles.appuser import AppUser
 
@@ -6,4 +7,19 @@ class CustomCreationFrom(UserCreationForm):
     class Meta:
         model = AppUser
         fields = ("email",)
-        
+
+
+class CustomAuthForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs={"autofocus": True, 'placeholder': 'Enter your email'}))
+    password = forms.CharField(
+        label=("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password", 'placeholder': 'Enter your password'}),
+    )
+    error_messages = {
+        "invalid_login": (
+            "Please enter a correct %(username)s and password. Note that both "
+            "fields may be case-sensitive."
+        ),
+        "inactive": ("This account is inactive."),
+    }
--- app_profiles/views.py
+++ app_profiles/views.py
@@ -5,9 +5,9 @@ from app_profiles.appuser import AppUser
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from app_profiles.forms import CustomCreationFrom
+from app_profiles.forms import CustomAuthForm, CustomCreationFrom
 
-
+# TODO view how to implement error msgs in the Login Form!
 class CreateUserView(CreateView):
     model = AppUser
     template_name = 'accounts/create_user.html'
@@ -15,15 +15,17 @@ class CreateUserView(CreateView):
     success_url = 'index page'
 
 class SignUpView(LoginView):
-    template_name = 'accounts/login.html'    
+    template_name = 'accounts/login.html'
+    form_class = CustomAuthForm    
     next_page = reverse_lazy('index page')
 
 class SignOutView(LogoutView):
     next_page = reverse_lazy('index page')
 
-class IndexView(ListView):
+class IndexView(LoginRequiredMixin, ListView):
     model = AppUser
     template_name = 'common/index.html'
+    login_url = 'login page'
     
     
 
